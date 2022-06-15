387fa3b2759ede0ed27bb4ee7aa8f2b7f1d7822e
eihigh <eihigh.contact@gmail.com>
2022-06-15 17:51:10 +0900

    add comments

--- assets.go
+++ assets.go
@@ -8,22 +8,35 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// embed機能 - Goには実行ファイルにさまざまなデータを埋め込めるembed機能があります。
+// ここでは assets ディレクトリの内容を FS 変数に埋め込んでいます。
+// embed.FS 型の変数の宣言の上に //go:embed （goの前にスペースがない）を書いて
+// コンパイラに指示を出すことで使えます。
+// 他にもファイルとしてではなく文字列として埋め込む機能などもありますが、割愛します。
+
 //go:embed assets
 var FS embed.FS
+
+// ファイルパスをキー、画像データを値としたmapに、ゲーム中で使う画像すべてを格納します。
 
 var (
 	images = map[string]*ebiten.Image{}
 )
 
 func loadImage(name string) error {
+	// embed.FSのメソッド ReadFile は、ファイルの内容を丸ごと []byte として読み込みます。
 	b, err := FS.ReadFile(name)
 	if err != nil {
 		return err
 	}
+	// データを画像にします。image.Decode 関数は io.Reader を要求するので、
+	// []byte を io.Reader として扱うために bytes.NewReader 関数を使います。
 	i, _, err := image.Decode(bytes.NewReader(b))
 	if err != nil {
 		return err
 	}
+	// 画像をEbitenの画像データに変換します。
+	// （内部的には、メインメモリからグラフィックメモリへデータがコピーされている）
 	e := ebiten.NewImageFromImage(i)
 	images[name] = e
 	return nil
--- main.go
+++ main.go
@@ -9,17 +9,24 @@ import (
 )
 
 const (
+	// 個人的な趣味で τ を使います。
 	tau = math.Pi * 2
 )
 
 var (
+	// 画面サイズです。どこでも使えるようにグローバル変数にすると便利です。
 	vw, vh = 400, 400
-	tick   = 0
-	frame  = 0
+	// Updateが呼ばれることを、Ebitenでは tick と呼びます。
+	tick = 0
+	// Draw が呼ばれることを、Ebitenでは frame と呼びます。
+	frame = 0
 )
 
+// ebiten.Game interface を満たす型がEbitenには必要なので、この game 構造体に
+// Update, Draw, Layout 関数を持たせます。
 type game struct{}
 
+// ゲームを初期化します。
 func newGame() (*game, error) {
 	g := &game{}
 
@@ -35,11 +42,16 @@ func newGame() (*game, error) {
 	return g, nil
 }
 
+// Update関数は、画面のリフレッシュレートに関わらず
+// 常に毎秒60回呼ばれます（既定値）。
+// 描画ではなく更新処理を行うことが推奨されます。
 func (g *game) Update() error {
 	tick++
 	return nil
 }
 
+// Draw関数は、画面のリフレッシュレートと同期して呼ばれます（既定値）。
+// 描画処理のみを行うことが推奨されます。ここで状態の変更を行うといろいろ事故ります。
 func (g *game) Draw(screen *ebiten.Image) {
 	frame++
 
@@ -52,10 +64,12 @@ func (g *game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(images["assets/jisyaku_bou.png"], op)
 }
 
+// Layout関数は、ウィンドウのリサイズの挙動を決定します。とりあえず常に画面サイズを返せば無難です。
 func (g *game) Layout(ow, oh int) (int, int) {
 	return vw, vh
 }
 
+// すべてのGoプログラムはmain packageのmain関数から始まります。
 func main() {
 	if err := _main(); err != nil {
 		panic(err)
@@ -67,7 +81,10 @@ func _main() error {
 	if err != nil {
 		return err
 	}
+	// ウィンドウタイトルを変更します。
 	ebiten.SetWindowTitle("Ebitengine Game Jam")
+	// ウィンドウサイズを決定します。
 	ebiten.SetWindowSize(vw, vh)
+	// ゲームスタート！
 	return ebiten.RunGame(g)
 }
