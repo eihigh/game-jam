8a9b21fddbc03c630e5c4b5bafd1703c3ed3f31c
eihigh <eihigh.contact@gmail.com>
2025-01-16 20:19:49 +0900

    crt shader

--- go.mod
+++ go.mod
@@ -1,26 +1,28 @@
 module github.com/eihigh/game-jam
 
-go 1.18
+go 1.22.0
 
-require github.com/hajimehoshi/ebiten/v2 v2.3.5
+toolchain go1.23.4
+
+require (
+	github.com/eihigh/canvas v0.0.2
+	github.com/fogleman/ease v0.0.0-20170301025033-8da417bf1776
+	github.com/hajimehoshi/ebiten/v2 v2.8.6
+)
 
 require (
 	github.com/ByteArena/poly2tri-go v0.0.0-20170716161910-d102ad91854f // indirect
 	github.com/benoitkugler/textlayout v0.1.3 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
-	github.com/eihigh/canvas v0.0.2 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20241016134836-cc2e38a7c0ee // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
 	github.com/flopp/go-findfont v0.1.0 // indirect
-	github.com/fogleman/ease v0.0.0-20170301025033-8da417bf1776 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220622232848-a6c407ee30a0 // indirect
-	github.com/gofrs/flock v0.8.1 // indirect
-	github.com/jezek/xgb v1.0.1 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/tdewolff/minify/v2 v2.11.10 // indirect
 	github.com/tdewolff/parse/v2 v2.6.0 // indirect
-	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
-	golang.org/x/exp/shiny v0.0.0-20220613132600-b0d781184e0d // indirect
-	golang.org/x/image v0.0.0-20220617043117-41969df76e82 // indirect
-	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
-	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
-	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -20,6 +20,8 @@ var (
 	tick = 0
 	// Draw が呼ばれることを、Ebitenでは frame と呼びます。
 	frame = 0
+
+	crtShader *ebiten.Shader
 )
 
 // ゲームを初期化します。
@@ -68,6 +70,21 @@ func (g *game) Layout(ow, oh int) (int, int) {
 	return vw, vh
 }
 
+func (g *game) DrawFinalScreen(screen ebiten.FinalScreen, offscreen *ebiten.Image, geoM ebiten.GeoM) {
+	if crtShader == nil {
+		var err error
+		crtShader, err = ebiten.NewShader([]byte(crtText))
+		if err != nil {
+			panic(err)
+		}
+	}
+	b := offscreen.Bounds()
+	opts := &ebiten.DrawRectShaderOptions{}
+	opts.Images[0] = offscreen
+	opts.GeoM = geoM
+	screen.DrawRectShader(b.Dx(), b.Dy(), crtShader, opts)
+}
+
 // すべてのGoプログラムはmain packageのmain関数から始まります。
 func main() {
 	if err := _main(); err != nil {
@@ -87,3 +104,60 @@ func _main() error {
 	// ゲームスタート！
 	return ebiten.RunGame(g)
 }
+
+const crtText = `
+//go:build ignore
+
+//kage:unit pixels
+
+// Based on https://github.com/XorDev/1PassBlur
+// https://www.shadertoy.com/view/DtscRf
+
+package main
+
+const radius = 16.0
+const samples = 32.0
+const base = 0.5
+const glow = 1.1
+
+func hash2(p vec2) vec2 {
+  return normalize(
+    fract(cos(p * mat2(195,174,286,183)) * 742) - 0.5)
+}
+
+func Fragment(dstPos vec4, srcPos vec2, color vec4) vec4 {
+  blur := vec4(0)
+  weights := 0.0
+  scale := radius / sqrt(samples)
+  offset := hash2(srcPos) * scale
+  rot := mat2(-0.7373688, -0.6754904, 0.6754904, -0.7373688)
+  for i := 0.0; i < samples; i += 1 {
+    // rotate by golden angle
+    offset *= rot
+    dist := sqrt(i)
+    pos := srcPos + offset*dist
+    color := imageSrc0At(pos)
+
+    weight := 1.0 / (1+dist)
+    blur += color * weight
+    weights += weight
+  }
+  blur /= weights
+  clr := mix(blur*glow, imageSrc0At(srcPos), base)
+
+  rgb := clr.rgb
+  rgb = clamp(mix(rgb, rgb*rgb, 0.4), 0, 1)
+
+  // vignette
+  origin, size := imageSrcRegionOnTexture()
+  uv := (srcPos - origin) / size
+  vig := 40*uv.x*uv.y*(1-uv.x)*(1-uv.y)
+  rgb *= vec3(pow(vig, 0.3))
+  rgb *= vec3(0.95, 1.05, 0.95)
+
+  rgb *= 1.0 - mod(dstPos.y, 2)*0.2
+
+  rgb *= 1.4
+  return vec4(rgb, clr.a)
+}
+`
